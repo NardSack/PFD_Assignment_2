cedee0aa68a7f0fbbe8c5ba394388f6f5ae3ec22
Lee Min Hui, Rachel <85004708+Rachelhu2710@users.noreply.github.com>
2024-01-18 13:36:12 +0800

    removing

--- main.js
+++ main.js
@@ -10,19 +10,6 @@ const requestListener = function (req, res) {
     res.writeHead(200);
     res.end(indexFile);
 };
-// const requestListener = function (req, res) {
-//     fs.readFile(__dirname + "/homepage.html")
-//         .then(contents => {
-//             res.setHeader("Content-Type", "text/html");
-//             res.writeHead(200);
-//             res.end(contents);
-//         })
-//         .catch(err => {
-//             res.writeHead(500);
-//             res.end(err);
-//             return;
-//         });
-// };
 
 const server = http.createServer(requestListener);
 
