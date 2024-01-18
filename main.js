4f23b9ec98358bd2a34a7f84a273077c08b67481
Giovanni Raynard Salim <S10242618@connect.np.edu.sg>
2024-01-18 14:12:50 +0800

    trying to add express to work

--- main.js
+++ main.js
@@ -24,3 +24,24 @@ fs.readFile(__dirname + "/homepage.html")
         console.error(`Could not read index.html file: ${err}`);
         process.exit(1);
     });
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+// Helper function to add a user to a SendBird channel
+app.post("/hand_off", async(req, res) => {
+    console.log(req.body);
+    const channelUrl = req.body.channel_url;
+
+    try {
+        const updateResponse = await updateTicketStatus(channelUrl);
+        
+        if (updateResponse.success) {
+            res.status(200).send({ "message": "Handing over to a human. Just a minute please." });
+        } else {
+            throw new Error(updateResponse.message);
+        }
+    } catch (e) {
+        res.status(400).send({ "error": true, "message": e.message || "Failed to perform hand over" });
+    }
+});
